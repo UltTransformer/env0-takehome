802d44b9581d1ce3148dc773112ffb457ea6b2a5
Chris Noon <36542253+UltTransformer@users.noreply.github.com>
2022-04-12 15:04:51 +0200

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -1,5 +1,13 @@
 terraform {
   required_providers {
+    random = {
+      source  = "hashicorp/random"
+      version = "3.1.0"
+    }
+    local = {
+      source  = "hashicorp/local"
+      version = "2.1.0"
+    }
     aws = {
       source = "hashicorp/aws"
     }
