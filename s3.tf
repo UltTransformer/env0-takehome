4983fae1626ce615c5e867ae8be9b0122ab9d027
Chris Noon <36542253+UltTransformer@users.noreply.github.com>
2022-04-12 12:52:03 +0200

    Update s3.tf

--- s3.tf
+++ s3.tf
@@ -1,13 +1,7 @@
-resource "aws_s3_bucket" "b" {
-  bucket = "my-tf-test-bucket"
-
-  tags = {
-    Name        = "My bucket"
-    Environment = "Dev"
-  }
-}
-
-resource "aws_s3_bucket_acl" "example" {
-  bucket = aws_s3_bucket.b.id
+resource "aws_s3_bucket" "cns3bucket" {
+  bucket = "cns3bucket"
   acl    = "private"
+  tags = {
+    Name = "cns3bucket"
+  }
 }
