638043ea1a949c830e148a7b90672fe1ffb71aab
Chris Noon <36542253+UltTransformer@users.noreply.github.com>
2022-04-12 14:56:29 +0200

    Update s3.tf

--- s3.tf
+++ s3.tf
@@ -1,6 +1,11 @@
 resource "aws_s3_bucket" "cns3bucket" {
   bucket = var.s3bucketname
   tags = {
-    Name = "cns3bucket"
+    Name = var.s3bucketname
   }
+}
+
+variable "s3bucketname" {
+  type    = string
+  default = "env0_bucket"
 }
