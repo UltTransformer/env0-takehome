ca0f74d4532389c2fdde040d8c5b16851f70ae7e
Chris Noon <36542253+UltTransformer@users.noreply.github.com>
2022-04-12 15:31:17 +0200

    Update s3.tf

--- s3.tf
+++ s3.tf
@@ -1,6 +1,6 @@
 resource "aws_s3_bucket" "cns3bucket" {
-  bucket = data.random_string.random.id
+  bucket = resource.random_string.random.id
   tags = {
-    Name = data.random_string.random.id
+    Name = resource.random_string.random.id
   }
 }
