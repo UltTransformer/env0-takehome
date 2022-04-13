b26c0dcdac00ca46d9a5a6acf10b7f106d98eb17
Chris Noon <36542253+UltTransformer@users.noreply.github.com>
2022-04-13 14:37:00 +0200

    Update s3.tf

--- s3.tf
+++ s3.tf
@@ -1,6 +1,6 @@
 resource "aws_s3_bucket" "cns3bucket" {
-  bucket = local.random_string
+  bucket = "env0takehome${local.random_string}"
   tags = {
-    Name = local.random_string
+    Name = "env0takehome${local.random_string}"
   }
 }
